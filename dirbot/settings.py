6d1dcbf12b2d75ccded98b270ff2e756a81b7cf3
sdai817 <flypig24241@gmail.com>
2018-08-08 17:25:55 -0700

    Update settings.py

--- dirbot/settings.py
+++ dirbot/settings.py
@@ -4,11 +4,11 @@ SPIDER_MODULES = ['dirbot.spiders']
 NEWSPIDER_MODULE = 'dirbot.spiders'
 DEFAULT_ITEM_CLASS = 'dirbot.items.Website'
 
-ITEM_PIPELINES = [
+ITEM_PIPELINES = {
     'dirbot.pipelines.RequiredFieldsPipeline',
     'dirbot.pipelines.FilterWordsPipeline',
     'dirbot.pipelines.MySQLStorePipeline',
-]
+}
 
 MYSQL_HOST = 'localhost'
 MYSQL_DBNAME = 'dirbot'
